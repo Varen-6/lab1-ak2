cc57b281c79a4350f2dfb7e003c77419ca49f967
Varen-6 <ergo1234@ukr.net>
2021-10-06 19:35:09 +0300

    my own changes

--- calculator.cpp
+++ calculator.cpp
@@ -13,4 +13,16 @@ int Calculator::Sub (double a, double b)
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
+	/*
+	AK-47
+	AKM
+	AK-74
+	AK-74U
+	AK-74M
+	AK-101
+	AK-103
+	AK-107
+	
+	AK-2
+	*/
 }
